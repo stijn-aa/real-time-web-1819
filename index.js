94b1560dd64418b730e659e8fa48101a32f57fd6
Stijn Aa <stijn.aa@gmail.com>
2019-04-09 16:30:38 +0200

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require("express")
 var path = require("path")
 var app = express();
-var server = require('https').createServer(app)
+const axios = require('axios')
+var server = require('http').createServer(app)
 var io = require('socket.io')(server)
 
 let userCount = 0
@@ -12,19 +13,30 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+axios.post('/webhook', function (req, res) {
+
+    const name = req.body.queryResult.parameters.any
+    io.emit('chat message', "google zegt" + name);
+    
+})
+.catch((error) => {
+    console.error(error)
+    io.emit('chat message', "google zegt" + error);
+});
+
 
 io.on('connection', function (socket) {
     console.log('a user connected');
     io.emit('chat message', 'user connected');
 
-    userCount ++;
+    userCount++;
     io.emit('usercount', userCount);
 
     socket.on('disconnect', function () {
         console.log('user disconnected');
-        io.emit('chat message', 'user disconnected'); 
-        userCount --;
-    io.emit('usercount', userCount);
+        io.emit('chat message', 'user disconnected');
+        userCount--;
+        io.emit('usercount', userCount);
     });
     socket.on('chat message', function (msg) {
         console.log('message: ' + msg);
