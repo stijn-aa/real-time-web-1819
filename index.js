65582b654eb02b9545631f2af2084b81d2fad837
Stijn Aa <stijn.aa@gmail.com>
2019-04-10 18:19:01 +0200

    make it better

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ var bodyParser = require('body-parser');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+const {
+    dialogflow,
+    actionssdk,
+    Image,
+    Table,
+    Carousel,
+} = require('actions-on-google');
+const reqproces = dialogflow({
+    debug: true
+});
+
 
 let userCount = 0
 
@@ -14,14 +25,12 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/webhook', function (req, res) {
 
-    const name = req.body.queryResult.parameters.any
-    io.emit('chat message', "google zegt " + name);
-    console.log(req.body)
-    res.end();
-    
-})
+
+reqproces.intent('fillIn', (conv, params) => {
+    conv.ask(`How are you, ${params.any}?`);
+});
+
 
 io.on('connection', function (socket) {
     console.log('a user connected');
@@ -43,8 +52,15 @@ io.on('connection', function (socket) {
 
 });
 
+app.post('/webhook', function (req, res) {
+
+    const msg = req.body.queryResult.parameters.any
+    io.emit('chat message', "google zegt " + msg);
+    console.log(req.body)
+    res.end();
+})
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 })
-
